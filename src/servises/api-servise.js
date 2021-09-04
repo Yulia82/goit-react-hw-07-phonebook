b7eca17e281b75a3bb4e609b1477ac255135e0e0
Yulia Shcherban <caribywest@gmail.com>
2021-09-04 21:58:13 +0300

    phonebook code

--- src/servises/api-servise.js
+++ src/servises/api-servise.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "http://localhost:7777";
-axios.defaults.baseURL =
-  "https://my-json-server.typicode.com/Yulia82/myJSONserver";
+axios.defaults.baseURL = "http://localhost:7777";
 
 export async function getContacts() {
   const { data } = await axios.get("/contacts");
